4549f20f60bdd4732c080182829aba531f3e2125
Lab Member 3 <jeffreyho1211@gmail.com>
2021-02-02 21:23:09 -0500

    Fetching individual stock from yahoo

--- js/stock_chart.js
+++ js/stock_chart.js
@@ -1,9 +1,26 @@
 var fetch_base_url = "https://query2.finance.yahoo.com/v10/finance/quoteSummary/"
 var tickers_url = "https://bigmoneymoves.github.io/data/tickers.txt"
 
-async function stock_fetch(){
-  var tickers = await getData(tickers_url);
-  console.log(tickers);
+var all_stock_data = {"tickers": []}
+
+async function fetch_stocks(){
+  var ticker_json = await getData(tickers_url);
+  tickers = ticker_json.tickers;
+
+  for (var i = 0; i < tickers.length; i++) {
+    await fetch_stock(tickers[i]);
+  }
+
+  return
+}
+
+async function fetch_stock(ticker){
+  var full_url = fetch_base_url + ticker + "?modules=price";
+  var ticker_json = await getData(full_url);
+  all_stock_data.tickers.push(ticker_json);
+
+  document.getElementById("stockChart").innerHTML = ticker_json;
+  return
 }
 
 async function getData(url){
@@ -11,6 +28,6 @@ async function getData(url){
   return response.json();
 }
 
-stock_fetch();
+fetch_stock("GME");
 
 //setInterval(stock_fetch, 1000);
