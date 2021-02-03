0b2e1891f6a50af0ca0be766536f065f84e4d4be
Lab Member 3 <jeffreyho1211@gmail.com>
2021-02-03 03:17:32 -0500

    Changed stock_chart to use python workaround

--- js/stock_chart.js
+++ js/stock_chart.js
@@ -1,8 +1,6 @@
-var fetch_base_url = "https://query2.finance.yahoo.com/v10/finance/quoteSummary/"
+var fetch_url = "https://bigmoneymoves.github.io/data/stocks.json"
 var tickers_url = "https://bigmoneymoves.github.io/data/tickers.txt"
-var proxy_url = 'https://plain-hat-031d.deraphel.workers.dev/?uri'
-
-var all_stock_data = {"tickers": []}
+var all_stock_data = {}
 
 async function add_ticker(ticker){
   return
@@ -12,57 +10,11 @@ async function remove_ticker(ticker){
   return
 }
 
-async function check_ticker(ticker){
-  var query_url = proxy_url + fetch_base_url + ticker + "?modules=price";
-  var check_json = await checkData(query_url);
-
-  return check_json.ok
-}
-
 async function fetch_stocks(){
-  var ticker_json = await getData(tickers_url);
-  tickers = ticker_json.tickers;
-
-  for (var i = 0; i < tickers.length; i++) {
-    await fetch_stock(tickers[i]);
-  }
-
+  all_stock_data = await getData(fetch_url);
+  console.log(all_stock_data)
   update_chart();
 
-  return
-}
-
-async function fetch_stock(ticker){
-  //
-
-  var full_url = proxy_url + fetch_base_url + ticker + "?modules=price";
-  var ticker_json = await getData(full_url);
-  var ticker_data = ticker_json.quoteSummary.result[0].price
-  //all_stock_data.tickers.push({[ticker]: ticker_json.quoteSummary.result[0].price});
-
-  if (typeof ticker_data.regularMarketOpen == "object") {
-    openPrice = ticker_data.regularMarketOpen.fmt
-    curPrice = ticker_data.regularMarketPrice.fmt
-    priceChange = ticker_data.regularMarketChange.fmt
-    priceChangePercent = ticker_data.regularMarketChangePercent.fmt
-    volume = ticker_data.regularMarketVolume.fmt
-  }
-
-  else {
-    openPrice = ticker_data.regularMarketOpen
-    curPrice = ticker_data.regularMarketPrice
-    priceChange = ticker_data.regularMarketChange
-    priceChangePercent = ticker_data.regularMarketChangePercent
-    volume = ticker_data.regularMarketVolume
-  }
-
-  relevant_data = [openPrice, curPrice, priceChange, priceChangePercent, volume]
-  all_stock_data.tickers.push({[ticker]: relevant_data})
-
-  console.log(`${openPrice}, ${curPrice}, ${priceChange}, ${priceChangePercent}, ${volume}`);
-  console.log(all_stock_data)
-
-  //document.getElementById("stockChart").innerHTML = testJSON;
   return
 }
 
@@ -71,19 +23,8 @@ async function getData(url){
   return response.json();
 }
 
-async function checkData(ticker){
-  try{
-    const response = await fetch("");
-    console.log(response.json());
-    return true;
-  }
-  catch{
-    console.log("NOPE");
-    return false;
-  }
+function update_chart(){
+  return
 }
 
-//fetch_stock("GME");
-checkData("GME")
-
-//setInterval(stock_fetch, 1000);
+fetch_stocks()
