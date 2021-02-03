4fa8e6a72faf6a047595d8ec3ff97284b2d12302
Lab Member 3 <jeffreyho1211@gmail.com>
2021-02-03 09:34:39 -0500

    Added stock table functionality

--- js/stock_chart.js
+++ js/stock_chart.js
@@ -1,6 +1,12 @@
-var fetch_url = "https://bigmoneymoves.github.io/data/stocks.json"
-var tickers_url = "https://bigmoneymoves.github.io/data/tickers.txt"
-var all_stock_data = {}
+var fetch_url = "https://bigmoneymoves.github.io/data/stocks.json";
+var tickers_url = "https://bigmoneymoves.github.io/data/tickers.txt";
+var all_stock_data = {};
+var table_headers = ["TICKER", "PREV CLOSE", "CUR PRICE", "CHANGE", "CHANGE %", "VOLUME"];
+var tickers;
+
+async function update_tickers(url){
+  tickers = await getData(tickers_url)
+}
 
 async function add_ticker(ticker){
   return
@@ -24,7 +30,29 @@ async function getData(url){
 }
 
 function update_chart(){
+  table = document.getElementById("stockTable");
+  for (var i = 0; i < tickers.tickers.length; i++) {
+    var ticker = tickers.tickers[i]
+    var ticker_data = all_stock_data[ticker]
+
+    var row = table.insertRow()
+    // ["TICKER", "PREV CLOSE", "CUR PRICE", "CHANGE", "CHANGE %", "VOLUME"]
+    var cell0 = row.insertCell(-1)
+    var cell1 = row.insertCell(-1)
+    var cell2 = row.insertCell(-1)
+    var cell3 = row.insertCell(-1)
+    var cell4 = row.insertCell(-1)
+    var cell5 = row.insertCell(-1)
+
+    cell0.innerHTML = ticker
+    cell1.innerHTML = ticker_data[0]
+    cell2.innerHTML = ticker_data[1]
+    cell3.innerHTML = ticker_data[2]
+    cell4.innerHTML = ticker_data[3]
+    cell5.innerHTML = ticker_data[4]
+  }
   return
 }
 
+update_tickers()
 fetch_stocks()
